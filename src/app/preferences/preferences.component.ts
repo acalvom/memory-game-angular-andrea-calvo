5a4d2adae37eba4c72cf620283b7b639061b354e
acalvom <andreac.3108@gmail.com>
2020-11-17 19:03:05 +0100

    add Preferences menu

--- src/app/preferences/preferences.component.ts
+++ src/app/preferences/preferences.component.ts
@@ -7,7 +7,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PreferencesComponent implements OnInit {
 
+  numberOfCards = 0;
+  timeLimit = 0;
+  preferences: number[] = [];
+
   constructor() { }
+
+  acceptPreferences(): void {
+    console.log(this.numberOfCards, this.timeLimit);
+    this.preferences = [this.numberOfCards, this.timeLimit];
+    console.log(this.preferences);
+    sessionStorage.setItem('preferences', String((this.preferences)));
+  }
 
   ngOnInit(): void {
   }
