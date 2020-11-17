c773cc1afea6e450aef486aeb620366404afefde
acalvom <andreac.3108@gmail.com>
2020-11-17 13:42:49 +0100

    add Log In menu

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-login',
@@ -7,7 +7,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  username = '';
+  password = '';
+  errorMsg = false;
+
+  constructor() {
+  }
+
+  loginUser(): void {
+    console.log(this.username);
+    console.log(this.password);
+
+  }
 
   ngOnInit(): void {
   }
