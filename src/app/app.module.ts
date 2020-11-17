5f444028c9eda079b0ea4ae109d7dedd8c6ecb8f
acalvom <andreac.3108@gmail.com>
2020-11-17 11:07:50 +0100

    add components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { StartComponent } from './start/start.component';
+import { PreferencesComponent } from './preferences/preferences.component';
+import { PlayComponent } from './play/play.component';
+import { RecordsComponent } from './records/records.component';
+import { LoginComponent } from './login/login.component';
+import { SigninComponent } from './signin/signin.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    StartComponent,
+    PreferencesComponent,
+    PlayComponent,
+    RecordsComponent,
+    LoginComponent,
+    SigninComponent,
   ],
   imports: [
     BrowserModule,
