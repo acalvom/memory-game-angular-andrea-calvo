f5780d28388ae4d4ae21926d4dd7537cfcbebbff
acalvom <andreac.3108@gmail.com>
2020-11-17 12:12:01 +0100

    add navigation bar & styles

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {StartComponent} from './start/start.component';
+import {PreferencesComponent} from './preferences/preferences.component';
+import {PlayComponent} from './play/play.component';
+import {RecordsComponent} from './records/records.component';
+import {LoginComponent} from './login/login.component';
+import {SigninComponent} from './signin/signin.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'start', component: StartComponent},
+  { path: 'preferences', component: PreferencesComponent},
+  { path: 'play', component: PlayComponent},
+  { path: 'records', component: RecordsComponent},
+  { path: 'login', component: LoginComponent},
+  { path: 'signin', component: SigninComponent},
+  { path: '', redirectTo: 'start', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
