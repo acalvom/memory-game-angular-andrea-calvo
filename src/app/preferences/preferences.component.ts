dac8631df0eedb21686b895bc0a38c1ab5dcea03
acalvom <andreac.3108@gmail.com>
2020-11-18 12:24:24 +0100

    reformat preferences.component.ts

--- src/app/preferences/preferences.component.ts
+++ src/app/preferences/preferences.component.ts
@@ -11,17 +11,14 @@ export class PreferencesComponent implements OnInit {
 
   numberOfCards = 0;
   timeLimit = 0;
-  //preferences: number[] = [];
 
   constructor(private router: Router, private preferences: PreferencesmanagerService) { }
 
   acceptPreferences(): void {
     console.log(this.numberOfCards, this.timeLimit);
     this.preferences.setPreferences(this.numberOfCards, this.timeLimit);
-    //this.preferences = [this.numberOfCards, this.timeLimit];
     console.log(this.preferences);
     this.router.navigate(['play']);
-    //sessionStorage.setItem('preferences', String((this.preferences)));
   }
 
   ngOnInit(): void {
